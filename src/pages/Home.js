37f9b0d8724964a143501c6b74e874d3372e689a
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2021-09-17 20:04:52 -0300

    Mensagem de boas vindas adicionada com useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { genericTypeAnnotation } from '@babel/types'
+import React, { useState, useEffect } from 'react'
 import { 
     View, 
     Text, 
@@ -14,15 +15,33 @@ import { SkillCard } from '../components/SkillCard'
 export function Home() {
     const [newSkill, setNewSkill] = useState('')
     const [mySkills, setMySkills] = useState([])
+    const [greeting, setGreeting] = useState('')
 
     function handleAddNewSkill() {
         //* Consigo recuperar o estado atual e adicionar um novo elemento no vetor
         setMySkills(oldState => [...oldState, newSkill])
     }
 
+    useEffect(() => {
+        const currentHour = new Date().getHours()
+
+        if(currentHour < 12) {
+            setGreeting('Good morning')
+        } else if (currentHour < 19) {     
+            setGreeting('Good afternoon')
+        } else {
+            setGreeting('Good night')
+        }
+
+    }, [greeting])
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome, Gabriel</Text>
+
+            <Text style={styles.greetings}>
+                {greeting}
+            </Text>
 
             <TextInput 
                 style={styles.input}
@@ -67,4 +86,7 @@ const styles = StyleSheet.create({
         marginTop: 30,
         borderRadius: 7,
     },
+    greetings: {
+        color: '#FFF',
+    },  
 })
