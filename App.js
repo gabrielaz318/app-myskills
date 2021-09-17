470da9f04dbbec5f3624e066b1349030ec11c72c
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2021-09-17 20:09:42 -0300

    Primeira etapa finalizada

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 
 import { Home } from './src/pages/Home'
 
 export default function App() {
-    return <Home />
+    return (
+        <>
+            <Home />
+        
+            <StatusBar barStyle="light-content" backgroundColor="#121015" />
+        </>
+    )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -62,7 +62,6 @@ export function Home() {
                 renderItem={({ item })=> <SkillCard  skill={item} />}
                 showsVerticalScrollIndicator={false}
             />
-
         </View>
     )
 }
